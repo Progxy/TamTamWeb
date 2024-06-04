cbb01c3d7fd50630167cc05c1d391a02213fa713
TheProgxy <theprogxy@gmail.com>
2024-06-04 23:36:49 +0200

    implement the stop of the tracking

--- home.ts
+++ home.ts
@@ -28,6 +28,8 @@ class Database {
 class MapClass {
     private readonly months : Array<String> = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];    
     private readonly days : Array<String> = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+    private markers: Array<any> = [];
+    private ids: Array<String> = [];
     private db: Database;
     private map: any;
 
@@ -55,15 +57,40 @@ class MapClass {
         const time: Date = new Date(data.lastUpdate);
         const time_str: String = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} on ${time.getDay()} ${this.days[time.getDate()]} ${this.months[time.getMonth()]} ${time.getFullYear()}`;
         this.map.setView(location, 13);
-        L.marker(location).addTo(this.map).bindPopup(`Victim coordinates:<br> Lat: ${location[0]},<br> Long: ${location[1]},<br> id: ${data.id},<br> isTracked: ${data.isTracked},<br> lastUpdate: ${time_str}`).openPopup();
-        
+        const marker = L.marker(location).addTo(this.map).bindPopup(`Victim coordinates:<br> Lat: ${location[0]},<br> Long: ${location[1]},<br> id: ${data.id},<br> isTracked: ${data.isTracked},<br> lastUpdate: ${time_str}`).openPopup();
+        this.markers.push(marker);
+        this.ids.push(id.value);
+
+        return;
+    }
+
+    stop() {
+        // Cast the HTMLElement to HTMLInputElement to use the value property without triggering the warning
+        let id : HTMLInputElement | null = <HTMLInputElement>document.getElementById("idInp");
+        if (id === null) {
+            console.log("Unable to find the text input!\n");
+            return;
+        }
+
+        const index: number = this.ids.indexOf(id.value);
+        if (index !== -1) {
+            this.map.removeLayer(this.markers[index]);
+            this.markers = this.markers.splice(index, index);
+            this.ids = this.ids.splice(index, index);
+        } else alert("Id not found!\n");
+
         return;
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const map: MapClass = new MapClass();
+    
     document.getElementById("locateBt")?.addEventListener("click", () => {
         map.locate();
+    });     
+
+    document.getElementById("stopBt")?.addEventListener("click", () => {
+        map.stop();
     }); 
 });
